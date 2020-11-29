7dd13a8787e284b0dfd8bf3c215374ac1f486db3
aozhouyuebing <1065605174@qq.com>
2020-11-29 15:17:20 +0800

    添加猫狗队列解法和栈排序解法

--- src/main/java/top/greatxiaozou/arithmeticQuestion/stackStackSort/Main.java
+++ src/main/java/top/greatxiaozou/arithmeticQuestion/stackStackSort/Main.java
@@ -1,6 +1,12 @@
 package top.greatxiaozou.arithmeticQuestion.stackStackSort;
 
 import java.util.*;
+
+/**
+ * 题目描述
+ * 一个栈中元素的类型为整型，现在想将该栈从顶到底按从大到小的顺序排序，
+ * 只许申请一个栈。除此之外，可以申请新的变量，但不能申请额外的数据结构。如何完成排序？
+ */
 public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
